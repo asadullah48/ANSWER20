51f80a4be56b8d68ee97a018f42ca26a6ffa8077
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-04-26 11:20:43 +0500

    changed

--- ans20.ts
+++ ans20.ts
@@ -1,9 +1,9 @@
 let languages: string[] = ["Arabic", "English", "Urdu",];
 console.log("languages which I can understand & speak frequenty:", languages);
 let countries: string[] = ["SAUDI ARABIA","OMAN", "KUWAIT", "BAHRAIN", "QATAR","Eypyt","Iraq", "jordan", "Yemen", "Japan", "Canada", "New Zealand", "Iceland", "Switzerland"];
-console.log("Countries I'd like to visit:", countries);
+console.log("Countries I want to visit & explore:", countries);
 let learning: string[] = ["typescipt", "Python", "Language of Quran", "Technical Skills", "Marketing"];
-console.log("learning I'd like to learn:", learning);
+console.log("Current goal I like to learn & implement:", learning);
 
 
 
